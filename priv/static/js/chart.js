a85ade0930b4a27f689c52219c42ba23d7f799f1
Namdak Tonpa <maxim@synrc.com>
2019-07-04 20:13:07 +0300

    chart.js

---
+++ priv/static/js/chart.js
@@ -0,0 +1,19 @@
+
+function draw_chart(id) {
+    var ctx = document.getElementById(id).getContext('2d');
+    new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+    options: {}
+});
+
+}
+
